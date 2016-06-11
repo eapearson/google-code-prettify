41d601f437f995d836759266e27723ecbe48d879
Erik Pearson <eapearson@lbl.gov>
2016-06-11 08:42:20 -0700

    reorganize code to be more in line with upstream: build tools moved back into tools directory

--- tools/closure-compiler/amd-externs.js
+++ tools/closure-compiler/amd-externs.js
@@ -1,8 +1,8 @@
 /**
- * @param {string} id
+ * 
  * @param {Array.<string>} dependencies
  * @param {Function} factory
  */
-function define(id, dependencies, factory) {}
+function define(dependencies, factory) {}
 /** @type {*} */
 define.amd;
